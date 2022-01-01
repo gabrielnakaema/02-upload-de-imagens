a52b7f43968ec3f12ef113ff262f3d6100d43895
Gabriel Nakaema <gsnakaema@gmail.com>
2022-01-01 20:02:52 -0300

    feat: view image modal

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,19 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent bgColor="pGray.800" w="fit-content" maxW="full">
+        <ModalBody w="fit-content" maxW="full" p={0}>
+          <Image src={imgUrl} alt="" maxW={900} maxH={600} objectFit="cover" />
+        </ModalBody>
+        <ModalFooter>
+          <Link href={imgUrl} target="_blank" mr="auto">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
